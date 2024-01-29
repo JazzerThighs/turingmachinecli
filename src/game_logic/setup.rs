14a1379eb612c430ac4c284ca4f1d3d7a1f9c96a
JazzerThighs <profenjb199@gmail.com>
2024-01-29 01:13:16 -0500

    modified:   src/game_logic/setup.rs 	modified:   src/main.rs

--- src/game_logic/setup.rs
+++ src/game_logic/setup.rs
@@ -1,14 +1,11 @@
-pub fn select_code_structure() -> (u32, u32, char, char) {
+use std::io;
+pub fn set_game_parameters() -> (u32, u32, char, char, String, u8) {
     let mut min_digit: char;
     let mut max_digit: char;
-    let mut min_code: u32 = 0;
-    let mut max_code: u32 = 0;
-
     loop {
-        use std::io;
         let mut input = String::new();
 
-        println!("Please input the smallest digit character. (In the original game, this is '1'.)");
+        println!("Please input the smallest digit character (In the original game, this is '1').");
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
@@ -21,7 +18,7 @@ pub fn select_code_structure() -> (u32, u32, char, char) {
         };
 
         let mut input = String::new();
-        println!("Please input the largest digit character. (In the original game, this is '5'.)");
+        println!("Please input the largest digit character (In the original game, this is '5').");
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
@@ -39,13 +36,17 @@ pub fn select_code_structure() -> (u32, u32, char, char) {
             );
             continue;
         }
-
+        break;
+    }
+    
+    let mut code_length: u8;
+    loop {
         let mut input = String::new();
-        println!("Please input the number of digits in the valid codes. (In the original game, this is 3, resulting in codes ranging from 111 to 555, inclusive.)");
+        println!("Please input the number of digits in the valid codes (In the original game, this is 3, resulting in codes ranging from 111 to 555, inclusive).");
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        let code_length: u8 = match input.trim().parse() {
+        code_length = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Invalid code length entered \"{}\"", input.trim());
@@ -53,24 +54,64 @@ pub fn select_code_structure() -> (u32, u32, char, char) {
             }
         };
         match code_length {
-            3 => {}
+            3 => break,
             _ => {
                 println!("Chosen length not implemented \"{}\"", code_length);
                 continue;
             }
         }
-
-        for _ in 1..=code_length {
-            min_code *= 10;
-            min_code += min_digit.to_digit(10).unwrap();
-            max_code *= 10;
-            max_code += max_digit.to_digit(10).unwrap();
-        }
-
+    }
+    
+    let mut min_code: u32 = 0;
+    let mut max_code: u32 = 0;
+    for _ in 1..=code_length {
+        min_code *= 10;
+        min_code += min_digit.to_digit(10).unwrap();
+        max_code *= 10;
+        max_code += max_digit.to_digit(10).unwrap();
+    }
+    
+    let mode: String;
+    loop {
+        let mut input = String::new();
+        println!("Please input the gamemode setting; Your choices are \"Classic Mode\"(c), \"Extreme Mode\"(e), and \"Nightmare Mode\"(n).");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        mode = match input.trim() {
+            "c" | "e" | "n" => input.trim().to_owned(),
+            _ => {
+                println!("Invalid mode selection \"{}\"", input.trim());
+                continue;
+            }
+        };
         break;
     }
+    
+    let mut test_amount: u8;
+    loop {
+        let mut input = String::new();
+        println!("Please input the number of tests that the puzzle shall contain (In the original game, there may be 4, 5, or 6 tests).");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        test_amount = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid test number entered \"{}\"", input.trim());
+                continue;
+            }
+        };
+        match test_amount {
+            4 | 5 | 6 => break,
+            _ => {
+                println!("Chosen number of tests not implemented \"{}\"", test_amount);
+                continue;
+            }
+        }
+    }
 
-    return (min_code, max_code, min_digit, max_digit);
+    return (min_code, max_code, min_digit, max_digit, mode, test_amount);
 }
 
 pub fn is_valid_turing_code(
@@ -150,3 +191,18 @@ pub fn target_code_index(matrix: Vec<TuringCodeResults>, target_code: u32) -> u3
     }
     return 0;
 }
+
+//  TODO: Generate the Puzzle by taking the generated random target_code, then selecting at least 4 tests which follow the schematic of the game, and setting up the user to play.
+//      PUZZLE GENERATION:
+//          -Generate a Vec<Vec<u32>> that has the same length as the results matrix. Each index contains a vector of test indexes that are coupled to that index's test.
+//          -Select a random test index which is true in the target_index's checks vector, and add it to the list of tests for the puzzle.
+//              -Ensure that the test index has been chosen based on the range specified by the selected difficulty:
+//                  Easy | Standard => Criteria Cards 1..=25,
+//                  Hard            => Criteria Cards 26..=48 for at least the first half of the tests, and 1..=48 for the rest of the tests.
+//              -Ensure that the test which has been selected hasn't had it's criteria card number added to the list of taken criteria cards.
+//              -Ensure that the test which has been selected is not included in any one of the previous tests' vector of coupled tests.
+//              If (the number of tests < the number of tests specified for the puzzle) {
+//                  for however many tests have been added to the puzzle's test list, make sure that the collection of tests chosen are not uniquely true for that one code.
+//              } Else If (the number of tests == the number of tests specified for the puzzle) {
+//                  ensure that the collection of tests is uniquely truthy for that one target_code, otherwise throw out the last code.
+//              }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use crate::{game_logic::*, setup::*};
 fn main() {
     println!("~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ \n\nWelcome to the Turing Machine CLI!\n\nThis program is a personal project based off of the board game called \"Turing Machine\" designed by Fabien Gridel & Yoann Levet.");
     
-    let (min_code, max_code, min_digit, max_digit) = setup::select_code_structure();
-    println!("Minimum Code: {}, Maximum Code: {}, Smallest Digit: {}, Largest Digit: {}", min_code, max_code, min_digit, max_digit);
+    let (min_code, max_code, min_digit, max_digit, mode, test_amount) = setup::set_game_parameters();
+    println!("Minimum Code: {}, Maximum Code: {},\nSmallest Digit: {}, Largest Digit: {},\nGamemode: {}, Amount of Tests: {}", min_code, max_code, min_digit, max_digit, mode, test_amount);
     let matrix: Vec<setup::TuringCodeResults> = generate_results_matrix(min_code, max_code, min_digit, max_digit);
     let target_code: u32 = generate_random_puzzle_code(min_code.to_string().len() as u32, min_digit, max_digit);
     let target_index: u32 = target_code_index(matrix, target_code);
