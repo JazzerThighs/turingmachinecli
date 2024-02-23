f05b02bb6f508fc7bf9893d446cd8e247686f273
JazzerThighs <profenjb199@gmail.com>
2024-02-22 20:18:39 -0500

    modified:   src/game_logic/game_variants/criteria_boilerplate.rs

--- src/game_logic/game_variants/criteria_boilerplate.rs
+++ src/game_logic/game_variants/criteria_boilerplate.rs
@@ -8,9 +8,9 @@ pub struct CriteriaCard {
 macro_rules! add_criteria_card {
     ($a:expr, $b:expr, $c:expr, $($d:expr),*) => {
         $a.push(CriteriaCard {
-                card_num: $b,
-                description: String::from($c),
-                tests: vec![$(String::from($d)),*],
+            card_num: $b,
+            description: String::from($c),
+            tests: vec![$(String::from($d)),*],
         });
     };
 }
